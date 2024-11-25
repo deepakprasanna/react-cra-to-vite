4fa86d961aac9b72a53043e591a4fcf3023eeead
Deepak Prasanna Ramaswami <deepak@Deepaks-MacBook-Pro.local>
2024-11-25 09:00:41 -0400

    feat: add react compiler

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const ReactCompilerConfig = {
+  target: '18', // '17' | '18' | '19'
+};
+
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-styled-components'],
+        plugins: [
+          'babel-plugin-styled-components',
+          ['babel-plugin-react-compiler', ReactCompilerConfig],
+        ],
       },
     }),
   ],
